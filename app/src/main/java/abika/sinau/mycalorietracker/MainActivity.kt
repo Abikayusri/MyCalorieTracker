e5f4609ccb8e0124dc8146d1a8068f6e4ee2c763
abikayusri <abikayusri96@gmail.com>
2022-09-03 01:59:26 +0700

    part1/setting_up_the_module_structure

--- app/src/main/java/abika/sinau/mycalorietracker/MainActivity.kt
+++ app/src/main/java/abika/sinau/mycalorietracker/MainActivity.kt
@@ -17,27 +17,27 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyCalorieTrackerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colors.background
+//                ) {
+//                    Greeting("Android")
+//                }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyCalorieTrackerTheme {
-        Greeting("Android")
-    }
-}
+//@Composable
+//fun Greeting(name: String) {
+//    Text(text = "Hello $name!")
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    MyCalorieTrackerTheme {
+//        Greeting("Android")
+//    }
+//}
